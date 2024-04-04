821b09d8307a82f1d18dd727bc8fb7f38c927242
LegendPhoenix66 <louis@hopp.lu>
2024-04-04 02:03:55 +0200

    added option to open obj files

--- src/main/kotlin/fortytwo/luxembourg/BasicFrame.kt
+++ src/main/kotlin/fortytwo/luxembourg/BasicFrame.kt
@@ -133,6 +133,35 @@ class BasicFrame(monitor: Int = -1) {
             println("$key: $action")
             if (key == GLFW_KEY_ESCAPE && action == GLFW_RELEASE) {
                 glfwSetWindowShouldClose(window, true)
+            } else if (key == GLFW_KEY_O && action == GLFW_RELEASE) {
+                // open file-chooser
+                java.awt.EventQueue.invokeLater {
+                    val chooser = javax.swing.JFileChooser()
+                    chooser.currentDirectory = java.io.File(".")
+                    val result = chooser.showOpenDialog(null)
+                    if (result == javax.swing.JFileChooser.APPROVE_OPTION) {
+                        val file = chooser.selectedFile
+                        println("Selected file: ${file.absolutePath}")
+
+                        // convert file to array of vertices and indices
+                        val objVertices = mutableListOf<Float>()
+                        val objIndices = mutableListOf<Int>()
+                        file.forEachLine {
+                            val parts = it.split(" ")
+                            if (parts[0] == "v") {
+                                objVertices.add(parts[1].toFloat())
+                                objVertices.add(parts[2].toFloat())
+                                objVertices.add(parts[3].toFloat())
+                            } else if (parts[0] == "f") {
+                                objIndices.add(parts[1].toInt() - 1)
+                                objIndices.add(parts[2].toInt() - 1)
+                                objIndices.add(parts[3].toInt() - 1)
+                            }
+                        }
+                        vertices = objVertices.toFloatArray()
+                        indices = objIndices.toIntArray()
+                    }
+                }
             }
         }
     }
