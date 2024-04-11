493c0971ec59242529d892c82331fafdaaf38cf3
LegendPhoenix66 <louis@hopp.lu>
2024-04-11 09:10:57 +0200

    for now just open in windowed mode

--- src/main/kotlin/fortytwo/luxembourg/Main.kt
+++ src/main/kotlin/fortytwo/luxembourg/Main.kt
@@ -1,12 +1,13 @@
 package fortytwo.luxembourg
 
 fun main() {
-    println("Hello, World!")
-    println("Select monitor:")
-    println("0: Primary")
-    println("1: Secondary")
-    println("-1: windowed mode")
-    val monitor = readln().toIntOrNull() ?: -1
+//    println("Hello, World!")
+//    println("Select monitor:")
+//    println("0: Primary")
+//    println("1: Secondary")
+//    println("-1: windowed mode")
+//    val monitor = readln().toIntOrNull() ?: -1
+    val monitor = -1
 
     val virtualThread = Thread.startVirtualThread { BasicFrame(monitor = monitor) }
     virtualThread.join()
